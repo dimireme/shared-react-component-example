ddb52b732c82d1acd7dd31f0cd6be7c8dc7cffd8
Alexander Shadrin <shadrinaa@altarix.ru>
2020-05-27 23:37:08 +0300

    Try to use rollup.

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonJs from '@rollup/plugin-commonjs';
+import typeScript from 'rollup-plugin-typescript2';
+import html from 'rollup-plugin-html';
+import visualizer from 'rollup-plugin-visualizer';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
+import { terser } from 'rollup-plugin-terser';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.r.min.js', format: 'iife' }],
+    plugins: [
+      nodeResolve(), // подключение модулей node
+      commonJs(), // подключение модулей commonjs
+      html(), // подключение html файлов
+      typeScript({ tsconfig: 'tsconfig.json' }), // подключение typescript
+      sizeSnapshot(), // напишет в консоль размер бандла
+      terser(), // минификатор совместимый с ES2015+, форк и наследник UglifyES
+      visualizer(), // анализатор бандла
+    ],
+  },
+];
